2f3ef71c47146e0d265b144c5ed73e4f16ee75f0
Bob <Bob@Bobs-MacBook-Pro.local>
2014-04-06 00:37:40 -0600

    changed names of folders

--- OSassignment_1/helloworld.c
+++
@@ -1,7 +0,0 @@
-/*This file defines the HelloWorld system call*/ 
-#include <linux/kernel.h>
-#include <linux/linkage.h>
-asmlinkage int sys_helloworld(){
-	printk(KERN_EMERG "rowe7280 says Hello World!\n"); 
-	return 0;
-}
